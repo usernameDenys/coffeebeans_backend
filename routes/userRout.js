abf2668fc3128a68f9b308788af35fc2322e9757
Denys <denysgolenko@gmail.com>
2025-06-02 01:27:21 +0200

    user account logic created

--- routes/userRout.js
+++ routes/userRout.js
@@ -3,12 +3,17 @@ import {
   loginUser,
   registUser,
   adminLogin,
+  getUserProfile,
+  updateUserProfile,
 } from "../controllers/userController.js";
+import { userAuth } from "../middleware/userAuthentication.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/admin", adminLogin);
+userRouter.get("/me", userAuth, getUserProfile);
+userRouter.patch("/me", userAuth, updateUserProfile);
 
 export default userRouter;
